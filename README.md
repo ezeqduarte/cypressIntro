28a40c337bade013db427899c5a6f43c32e16e8a
Ezequiel Duarte <ezeqduarte01@gmail.com>
2023-08-23 18:38:37 -0300

    first test with Cypress

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # cypressIntro
 Introduccion a Cypress. Test funcional
 
-## Pasos a seguir
+## Pasos a seguir desde el desarollo
 
 1- npm init
 2- npm install cypress --save-dev
-3- node_modules\.bin\cypress open
-4- npx cypress open
+3- npx cypress open
+
+---
+
+- Para poder parar la consola utilizamos control + C
+
+## Pasos a para clonarse el repositorio
+
+1- npm install
+2- npx cypress open
---
+++ cypress/e2e/section1/holaMundo.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+
+describe("Mi primer test con Cypress", () => {
+  it("Mi primer test => hola mundo", () => {
+      //console log
+      cy.log("Hola mundo")
+      //comando para esperar en milisegundos
+      cy.wait(2000)
+      //visita el sitio
+      cy.visit("https://testingqarvn.com.es/datos-personales")
+      cy.wait(2000)
+      //.get encuentra el elemento por su id y .type tipea un valor dentro del componente
+      cy.get("#wsf-1-field-21").type("Enzo")
+      cy.wait(2000)
+      cy.get("#wsf-1-field-22").type("Perez")
+      cy.wait(2000)
+      cy.get("#wsf-1-field-23").type("enzoperez@gmail.com")
+      cy.wait(2000)
+      cy.get("#wsf-1-field-24").type("+5491234568795")
+      cy.wait(2000)
+      cy.get("#wsf-1-field-28").type("El monumental es la direccion")
+      cy.wait(2000)
+      //click realizar la accion clickear al componente
+      cy.get("#wsf-1-field-27").click()
+      cy.wait(4000)
+    })
+})
+
+
+
+/* https://demoqa.com/text-box */
